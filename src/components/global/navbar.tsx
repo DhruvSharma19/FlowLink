dff20ddf39b83d64eca44d736904caa20f39cdde
Dhruv Sharma <dhruvamsh@gmail.com>
2024-06-01 10:03:56 +0530

    Changes

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async (props: Props) => {
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
-        <p className="text-3xl font-bold">Fu</p>
+        <p className="text-3xl font-bold">Flow</p>
         <Image
           src="/fuzzieLogo.png"
           width={15}
@@ -19,7 +19,7 @@ const Navbar = async (props: Props) => {
           alt="fuzzie logo"
           className="shadow-sm"
         />
-        <p className="text-3xl font-bold">zie</p>
+        <p className="text-3xl font-bold">Link</p>
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
         <ul className="flex items-center gap-4 list-none">
