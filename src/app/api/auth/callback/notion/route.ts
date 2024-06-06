ecaecf87d05c19638579d8b523dc30e7da6ba5dc
Dhruv Sharma <dhruvamsh@gmail.com>
2024-06-06 12:44:21 +0530

    Update url

--- src/app/api/auth/callback/notion/route.ts
+++ src/app/api/auth/callback/notion/route.ts
@@ -1,22 +1,22 @@
-import axios from 'axios';
-import { NextRequest, NextResponse } from 'next/server';
-import { Client } from '@notionhq/client';
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+import { Client } from "@notionhq/client";
 
 export async function GET(req: NextRequest) {
-  const code = req.nextUrl.searchParams.get('code');
+  const code = req.nextUrl.searchParams.get("code");
   const encoded = Buffer.from(
     `${process.env.NOTION_CLIENT_ID}:${process.env.NOTION_API_SECRET}`
-  ).toString('base64');
+  ).toString("base64");
   if (code) {
-    const response = await axios('https://api.notion.com/v1/oauth/token', {
-      method: 'POST',
+    const response = await axios("https://api.notion.com/v1/oauth/token", {
+      method: "POST",
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
         Authorization: `Basic ${encoded}`,
-        'Notion-Version': '2022-06-28',
+        "Notion-Version": "2022-06-28",
       },
       data: JSON.stringify({
-        grant_type: 'authorization_code',
+        grant_type: "authorization_code",
         code: code,
         redirect_uri: process.env.NOTION_REDIRECT_URI!,
       }),
@@ -27,25 +27,25 @@ export async function GET(req: NextRequest) {
       });
       const databasesPages = await notion.search({
         filter: {
-          value: 'database',
-          property: 'object',
+          value: "database",
+          property: "object",
         },
         sort: {
-          direction: 'ascending',
-          timestamp: 'last_edited_time',
+          direction: "ascending",
+          timestamp: "last_edited_time",
         },
       });
       const databaseId = databasesPages?.results?.length
         ? databasesPages.results[0].id
-        : '';
+        : "";
 
-        console.log(databaseId)
+      console.log(databaseId);
 
       return NextResponse.redirect(
-        `https://localhost:3000/connections?access_token=${response.data.access_token}&workspace_name=${response.data.workspace_name}&workspace_icon=${response.data.workspace_icon}&workspace_id=${response.data.workspace_id}&database_id=${databaseId}`
+        `https://flow-link.vercel.app//connections?access_token=${response.data.access_token}&workspace_name=${response.data.workspace_name}&workspace_icon=${response.data.workspace_icon}&workspace_id=${response.data.workspace_id}&database_id=${databaseId}`
       );
     }
   }
 
-  return NextResponse.redirect('https://localhost:3000/connections');
+  return NextResponse.redirect("https://flow-link.vercel.app//connections");
 }
--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,25 +1,25 @@
-import { NextResponse, NextRequest } from 'next/server'
-import Stripe from 'stripe'
+import { NextResponse, NextRequest } from "next/server";
+import Stripe from "stripe";
 
 export async function GET(req: NextRequest) {
   const stripe = new Stripe(process.env.STRIPE_SECRET!, {
     typescript: true,
-    apiVersion: '2023-10-16',
-  })
+    apiVersion: "2023-10-16",
+  });
 
   const products = await stripe.prices.list({
     limit: 3,
-  })
+  });
 
-  return NextResponse.json(products.data)
+  return NextResponse.json(products.data);
 }
 
 export async function POST(req: NextRequest) {
   const stripe = new Stripe(process.env.STRIPE_SECRET!, {
     typescript: true,
-    apiVersion: '2023-10-16',
-  })
-  const data = await req.json()
+    apiVersion: "2023-10-16",
+  });
+  const data = await req.json();
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
@@ -27,10 +27,10 @@ export async function POST(req: NextRequest) {
         quantity: 1,
       },
     ],
-    mode: 'subscription',
+    mode: "subscription",
     success_url:
-      'https://localhost:3000/billing?session_id={CHECKOUT_SESSION_ID}',
-    cancel_url: 'https://localhost:3000/billing',
-  })
-  return NextResponse.json(session.url)
+      "https://flow-link.vercel.app//billing?session_id={CHECKOUT_SESSION_ID}",
+    cancel_url: "https://flow-link.vercel.app//billing",
+  });
+  return NextResponse.json(session.url);
 }
